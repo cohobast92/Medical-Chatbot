5f0659b07955be4f01ece06f0e0997228a15c406
cohobast92 <cohobast92@users.noreply.github.com>
2026-03-24 09:33:42 -0400

    Update README

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical_chatbot',
+    version='0.1.0',
+    author='Sharvil Wadekar',
+    author_email='sharvilwadekar@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
